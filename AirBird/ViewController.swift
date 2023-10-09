0841763bf14618fa8051bc7aa65da84671f293f7
JusBug <pksgwo123@gmail.com>
2023-10-09 13:12:31 +0900

    Initial Commit

---
+++ AirBird/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirBird
+//
+//  Created by 박종화 on 2023/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
